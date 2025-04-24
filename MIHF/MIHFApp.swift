0605938e03472a5addfba968bb32574871f11b37
aadrobot <aadrobot@edu.hse.ru>
2025-04-24 22:03:04 +0300

    Initial Commit

---
+++ MIHF/MIHFApp.swift
@@ -0,0 +1,17 @@
+//
+//  MIHFApp.swift
+//  MIHF
+//
+//  Created by Alexey Drobot on 24.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct MIHFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
